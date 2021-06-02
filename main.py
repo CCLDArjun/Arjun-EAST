702411fcc5f8cbe174739d81d994397741d44e87
Arjun <31632587+CCLDArjun@users.noreply.github.com>
2021-06-02 11:23:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-from model import FeatureExtractor, EastOCR
-import torch
 
-v = FeatureExtractor()
-east_ocr: EastOCR = EastOCR(v)
-east_ocr.load_state_dict(torch.load('/Users/arjunbemarkar/Python/ArjunEAST/IncidentalSceneTextDataset/myeast_vgg16.pth', map_location=torch.device('cpu')))
-east_ocr.eval()
-d = east_ocr(torch.rand(1, 3, 224, 224))
